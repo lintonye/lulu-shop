932d37f07cd0500c6c9892e974da334358961bd7
lintonye <linton@jimulabs.com>
2023-06-24 12:37:09 -0700

    Cleaning data, update prompt

--- next-app/app/api/chat/route.ts
+++ next-app/app/api/chat/route.ts
@@ -2,6 +2,7 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import { productCatalog } from "./data";
+import { csvParse } from "d3-dsv";
 
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
@@ -12,6 +13,17 @@ const openai = new OpenAIApi(config);
 // IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
+function getProductCatalog() {
+  const data = csvParse(productCatalog);
+  const result = data.map(
+    (d) =>
+      `name: ${d["Product Title"]}\nurl: ${d["Origin URL"]}\ngender: ${d["Gender Cloths"]}\nprice: ${d["price"]}\nimage: ${d["Image URL"]}]}`
+  );
+  console.log(data.columns);
+  console.log(result);
+  return result;
+}
+
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
   const { messages } = await req.json();
@@ -19,10 +31,13 @@ export async function POST(req: Request) {
   // Ask OpenAI for a streaming chat completion given the prompt
   const systemMessage = {
     role: "system",
-    content: `You are LuluAI, an AI assistant at Lululemon. Products you carry include: ${productCatalog.slice(
-      0,
-      1000
-    )} `,
+    content: `You are LuluAI, an AI assistant at Lululemon. Show only products available in the product catalog (delimited with three dashes). Reply in markdown. When listing a product, always include a valid URL, for example [Product name](url). Make sure to include ONLY urls from the product catalog. Do not invent new products or urls.
+    
+    Product catalog
+  ---
+    ${getProductCatalog().join("\n")} 
+  ---
+    `,
   };
 
   const response = await openai.createChatCompletion({
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useChat } from "ai/react";
+import ReactMarkdown from "react-markdown";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
@@ -8,15 +9,29 @@ export default function Chat() {
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <div className="font-calibre text-3xl">Lululemon shop bot</div>
-      {messages.length > 0
-        ? messages.map((m) => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === "user" ? "User: " : "AI: "}
-              {m.content}
-            </div>
-          ))
-        : null}
-
+      <div className="flex flex-col gap-3">
+        {messages.length > 0
+          ? messages.map((m) => (
+              <div key={m.id} className="whitespace-pre-wrap">
+                <div className="text-sm">
+                  {m.role === "user" ? "User: " : "AI: "}
+                </div>
+                <div className="flex flex-col">
+                  <ReactMarkdown
+                    linkTarget="_blank"
+                    components={{
+                      a: ({ className, ...props }) => (
+                        <a {...props} className={`${className} underline`} />
+                      ),
+                    }}
+                  >
+                    {m.content}
+                  </ReactMarkdown>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
       <form onSubmit={handleSubmit}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
